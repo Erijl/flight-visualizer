9da4f9e3ea172668906f1ff703e7fa5debd712bd
Erijl <justusmau@web.de>
2024-05-25 09:30:06 +0200

    update loading animation to support multiple airplanes satisfyingly

--- flightvisualizer-frontend/src/app/loading-overlay/loading-overlay.component.ts
+++ flightvisualizer-frontend/src/app/loading-overlay/loading-overlay.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-loading-overlay',
   templateUrl: './loading-overlay.component.html',
-  styleUrls: ['./loading-overlay.component.css']
+  styleUrls: ['./loading-overlay.component.scss']
 })
 export class LoadingOverlayComponent {
   @Input() zIndex = 50;
