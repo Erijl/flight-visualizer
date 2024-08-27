13d6df0466007c3e4a2d1a3bd1642f7a6dfdd04e
Erijl <justusmau@web.de>
2024-08-27 22:24:52 +0200

    hotfix: update mapbox token

--- flightvisualizer-frontend/src/environments/environment.development.ts
+++ flightvisualizer-frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiEndpoint: 'http://localhost:8081/',
-  mapboxAccessToken: 'pk.eyJ1IjoiZXJpamwiLCJhIjoiY2x1bHk5eGxyMTV4YTJrbG12NmVnbGwycyJ9.iETWm_BL7w2AdhdulOW16Q',
+  mapboxAccessToken: 'pk.eyJ1IjoiZXJpamwiLCJhIjoiY2xzejRzZXpuMGx2MzJxcDI4cXRzaTh4ZSJ9.Smf0UR38mewgm12ORBGymQ',
 };
